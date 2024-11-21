8836017c18d72047930553b5e9f780878ea1417e
omegatech-andre <omega.ti_ind@outlook.com>
2024-11-21 14:51:16 -0300

    fix: adicionando autenticação na area de login

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -54,6 +54,10 @@ const authOptions: NextAuthOptions = {
     })
   ],
   callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.user = user;
+      return token;
+    },
     async session({ session, token }) {
       session.user = token.user as any;
       return session;
--- src/components/pages/auth/signIn.tsx
+++ src/components/pages/auth/signIn.tsx
@@ -1,8 +1,10 @@
+import NotificationShow from "@/components/_ui/notification/notificationShow";
 import { schemaAuth } from "@/schemas/auth/schemaAuth";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Button, Group, PasswordInput, TextInput } from "@mantine/core";
 import { signIn } from "next-auth/react";
 import { redirect } from "next/navigation";
+import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 interface UsePostReq {
@@ -10,59 +12,67 @@ interface UsePostReq {
   USER_PASSWORD: string;
 }
 
-interface UsePostRes {
-  access_token: string;
-}
-
 export default function Signin() {
-  const { register, handleSubmit, watch } = useForm({
+  const [isLoading, setIsLoading] = useState(false);
+  const { register, handleSubmit, formState: { errors } } = useForm({
     mode: 'onChange',
     resolver: yupResolver(schemaAuth),
   });
 
   const submitForm: SubmitHandler<UsePostReq> = async (formData) => {
+    setIsLoading(true);
     const login = await signIn('credentials', {
       USER_NAME: formData.USER_NAME,
       USER_PASSWORD: formData.USER_PASSWORD,
       redirect: false,
     });
+    setIsLoading(false);
 
-    if (login?.ok) redirect('/produtos')
+    if (login?.ok) {
+      NotificationShow({
+        title: 'Sucesso',
+        message: 'Usuário logado com sucesso!',
+      });
+      redirect('/produtos');
+    }
+
+    if (login?.error) {
+      if (login.status === 401) {
+        NotificationShow({
+          title: 'Erro de Login',
+          message: 'Usuário ou senha incorretos.',
+        });
+      } else {
+        NotificationShow({
+          title: 'Erro',
+          message: 'Ocorreu um erro ao tentar fazer o login. Tente novamente mais tarde.',
+        });
+      }
+    }
   };
-
-  // useEffect(() => {
-  //   if (error) {
-  //     NotificationShow({
-  //       title: 'Erro',
-  //       message: 'Ocorreu um erro ao tentar fazer o login.',
-  //     });
-  //   }
-
-  //   if (response) {
-  //     NotificationShow({
-  //       title: 'Sucesso',
-  //       message: 'Usuário logado com sucesso!',
-  //     });
-  //   }
-  // }, [error, response]);
 
   return (
     <form onSubmit={handleSubmit(submitForm)}>
       <TextInput
         {...register('USER_NAME')}
         label="Username"
+        error={errors.USER_NAME?.message}
+        aria-label="Nome de usuário"
+        autoComplete="username"
+        required
       />
       <PasswordInput
         {...register('USER_PASSWORD')}
         label="Senha"
+        error={errors.USER_PASSWORD?.message}
+        aria-label="Senha"
+        autoComplete="current-password"
+        required
       />
       <Group justify="flex-end" mt="md">
-        <Button fullWidth type="submit">
+        <Button fullWidth type="submit" disabled={isLoading} loading={isLoading}>
           Entrar
         </Button>
-        {/* <Button fullWidth type="submit" disabled={isPosting} loading={isPosting}>
-          Entrar
-        </Button> */}
       </Group>
     </form>
   );
