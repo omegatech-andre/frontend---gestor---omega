c99ca52c75f1f93fb9a368278c3edfee380f79d9
omegatech-andre <omega.ti_ind@outlook.com>
2024-12-16 11:52:04 -0300

    feat: adicionando productDetails.ts

---
+++ src/types/productDetails.ts
@@ -0,0 +1,43 @@
+import { FileWithPath } from "@mantine/dropzone";
+
+export interface ProductGetDetails {
+  id: string;
+  PRODUCT_NAME: string;
+  PRODUCT_DESCRIPTION: string;
+  PRODUCT_FISPQ: string;
+  PRODUCT_BOLETIM: string;
+  PRODUCT_URL_IMAGES: string[];
+  PRODUCT_TAGS: string[];
+  PRODUCT_COLORS: {
+    COLOR_NAME: string;
+    COLOR_HEX: string;
+  }[];
+  PRODUCT_SIZES: string[];
+  PRODUCT_STATUS: 'PENDING' | 'APPROVED' | 'DISABLED';
+  PRODUCT_CATEGORY: {
+    CATEGORY_NAME: string;
+    CATEGORY_LINE: {
+      LINE_NAME: string;
+    };
+  };
+  createdAt: string;
+}
+
+export interface ProductPostDetails {
+  PRODUCT_NAME?: string;
+  PRODUCT_DESCRIPTION?: string;
+  PRODUCT_FISPQ?: string;
+  PRODUCT_BOLETIM?: string;
+  PRODUCT_TAGS?: string[];
+  PRODUCT_COLORS?: {
+    COLOR_NAME: string;
+    COLOR_HEX: string;
+  }[];
+  PRODUCT_SIZES?: string[];
+  PRODUCT_STATUS?: 'PENDING' | 'APPROVED' | 'DISABLED';
+  FK_PRODUCT_CATEGORY?: string;
+}
+
+export interface ProductPostImage {
+  PRODUCT_URL_IMAGES: FileWithPath;
+}
